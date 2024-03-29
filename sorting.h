695eba3fffd106f977fde9bc64e2e60bfac52d30
Meera Kurup <145291848+meerak2110@users.noreply.github.com>
2024-03-29 01:06:45 -0400

    header file

---
+++ sorting.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void bubbleSort(int arr[], int n);
+void heapify(int arr[], int n, int i);
+void heapSort(int arr[], int n);
